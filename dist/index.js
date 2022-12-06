7c6bd3179db08579450f1b95172c3d04b6ab8670
AkshaySharma-BharatP <akshay.sharma@bharatpe.com>
2022-12-06 14:03:15 +0530

    test 1

--- dist/index.js
+++ dist/index.js
@@ -13346,7 +13346,10 @@ const main = async () => {
     }
 
     const getTableDataString = (filteredFiles) => {
-      const res = `|File Name|File Size|\n|-----|:-----:|`;
+      const res = `|File Name|File Size|\n|-----|:-----:|\n|${filteredFiles[0][0]}|${filteredFiles[0][1]}`;
+      for(let item of filteredFiles) {
+        console.log(item[0], item[1]);
+      }
       return res;
     };
  
