d0e530b1d6f6b970f264d2a69fc440da7431cca3
AkshaySharma-BharatP <akshay.sharma@bharatpe.com>
2022-12-06 12:54:17 +0530

    test 1

--- index.js
+++ index.js
@@ -44,11 +44,9 @@ const main = async () => {
     };
 
     await exec.exec(`find ${inputs.target_folder} -type f \( -name "*.jpeg" -o -name "*.png" -o -name "*.svg" -o -name "*.gif" -o -name "*.jpg" \) -size +${inputs.thrashold_size}k -exec ls -lh {} \;`, null, options);
-    // await exec.exec(`find ${inputs.target_folder} \( -iname '*.gif' -o -iname '*.jpg' -o -iname '*.svg' -o -iname '*.jpeg' -o -iname '*.png' \) -type f -size +${inputs.thrashold_size}k -exec ls -lh {} \;`, null, options);
-    // await exec.exec(`find ${inputs.target_folder} -name *.jpeg -type f -size +1000k -exec ls -lh {} \;`)
 
-    // await exec.exec(`find ${inputs.target_folder} -type f \( -name "*.jpeg" -o -name "*.png" -o -name "*.svg" \) -exec ls -lh {} \;`)
-    // await exec.exec(`find ${inputs.target_folder} -type f \( -name "*.jpeg" -o -name "*.png" -o -name "*.svg" \) -size +10k -exec ls -lh {} \;`)
+    const data =  await exec.exec(`find ${inputs.target_folder} -type f \( -name "*.jpeg" -o -name "*.png" -o -name "*.svg" -o -name "*.gif" -o -name "*.jpg" \) -size +${inputs.thrashold_size}k -exec ls -lh {} \;`);
+    console.log('-----', data);
 
 
     const arrayOutput = myOutput.split("\n");
@@ -57,7 +55,6 @@ const main = async () => {
     const successBody = ` Woohooo :rocket: !!! Congratulations, your all assets are less than ${inputs.thrashold_size}Kb.`
     const errorBody = `Oops :eyes: !!! You have ${count} assets with size more than ${inputs.thrashold_size}Kb. Please optimize them.`
 
-    console.log('-------', arrayOutput)
     if(count > 0) {
       octokit.rest.issues.createComment({
         owner,
